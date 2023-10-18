efbd3f7981911a55a2e3629b8f9425f83ab6e927
Ruan Victor <ruanvictormr@gmail.com>
2023-10-18 19:12:48 -0300

    feat: helper function product price

---
+++ src/helpers/product.ts
@@ -0,0 +1,18 @@
+import { Product } from "@prisma/client";
+
+const computedProductTotalPrice = (product: Product) => {
+  if (product.discountPercentage === 0) {
+    return {
+      ...product,
+      totalPrice: Number(product.basePrice),
+    };
+  }
+
+  const totalPrice =
+    Number(product.basePrice) * (product.discountPercentage / 100);
+
+  return {
+    ...product,
+    totalPrice,
+  };
+};
