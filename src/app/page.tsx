80519c8c65f215088e6c343d285a10b18e6faa8f
Ruan Victor <ruanvictormr@gmail.com>
2023-10-17 21:48:00 -0300

    feat: auth and auth handler to header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+
 export default function Home() {
+  const { data } = useSession();
   return (
     <main>
-      <h1>Hello Word</h1>
+      <pre>{data?.user?.email}</pre>
     </main>
   );
 }
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,7 +1,9 @@
+"use client";
 import {
   HomeIcon,
   ListOrderedIcon,
   LogInIcon,
+  LogOutIcon,
   MenuIcon,
   PercentIcon,
   ShoppingCart,
@@ -9,8 +11,19 @@ import {
 import { Button } from "./button";
 import { Card } from "./card";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./avatar";
+import { Separator } from "./separator";
 
 const Header = () => {
+  const { status, data } = useSession();
+  const handleLoginClick = async () => {
+    await signIn();
+  };
+
+  const handleLogoutClick = async () => {
+    await signOut();
+  };
   return (
     <Card className="flex items-center justify-between p-[1.875rem]">
       <Sheet>
@@ -25,11 +38,45 @@ const Header = () => {
             Menu
           </SheetHeader>
 
-          <div className="mt-2 flex flex-col gap-3">
-            <Button variant="outline" className="w-full justify-start gap-1">
-              <LogInIcon />
-              Fazer Login
-            </Button>
+          {status === "authenticated" && data.user && (
+            <div className="flex flex-col">
+              <div className="flex items-center gap-2 py-4">
+                <Avatar>
+                  <AvatarFallback>
+                    {data.user.name?.[0].toUpperCase()}
+                  </AvatarFallback>
+
+                  {data.user.image && <AvatarImage src={data.user.image} />}
+                </Avatar>
+
+                <p className="font-medium">{data.user.name}</p>
+              </div>
+              <Separator />
+            </div>
+          )}
+
+          <div className="mt-4 flex flex-col gap-3">
+            {status === "unauthenticated" && (
+              <Button
+                variant="outline"
+                className="w-full justify-start gap-1"
+                onClick={handleLoginClick}
+              >
+                <LogInIcon size={16} />
+                Fazer Login
+              </Button>
+            )}
+
+            {status === "authenticated" && (
+              <Button
+                variant="outline"
+                className="w-full justify-start gap-1"
+                onClick={handleLogoutClick}
+              >
+                <LogOutIcon size={16} />
+                Logout
+              </Button>
+            )}
 
             <Button variant="outline" className="w-full justify-start gap-1">
               <HomeIcon size={16} />
