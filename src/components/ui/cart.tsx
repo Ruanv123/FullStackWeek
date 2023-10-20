b1714f3aeccef93df669e91840f29824843ac0d6
Ruan Victor <ruanvictormr@gmail.com>
2023-10-19 22:51:55 -0300

    feat: scroll area no carrinho

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -5,11 +5,13 @@ import { CartContext } from "@/providers/cart";
 import CartItem from "./cart-item";
 import { computedProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
 
 const Cart = () => {
   const { products, total, subtotal, totalDiscounts } = useContext(CartContext);
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -19,17 +21,21 @@ const Cart = () => {
       </Badge>
 
       {/* renderizar os produtso */}
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-          products.map((product) => (
-            <CartItem
-              product={computedProductTotalPrice(product as any) as any}
-              key={product.id}
-            />
-          ))
-        ) : (
-          <p className="text-center font-semibold">Carrinho vazio.</p>
-        )}
+      <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-4">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  product={computedProductTotalPrice(product as any) as any}
+                  key={product.id}
+                />
+              ))
+            ) : (
+              <p className="text-center font-semibold">Carrinho vazio.</p>
+            )}
+          </div>
+        </ScrollArea>
       </div>
 
       <div className="flex flex-col gap-3">
@@ -59,6 +65,8 @@ const Cart = () => {
           <p>Total</p>
           <p>R$ {total.toFixed(2)}</p>
         </div>
+
+        <Button className="mt-7 font-bold uppercase">Finalizar compra</Button>
       </div>
     </div>
   );
